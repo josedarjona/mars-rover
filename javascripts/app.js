c8a6d171067df220f73b176b043aafbd1d694a67
jose arjona <josedarjona@gmail.com>
2018-04-02 23:12:20 -0400

    update

--- javascripts/app.js
+++ javascripts/app.js
@@ -3,86 +3,84 @@
 
 var rover = {
   direction: "n",
-  x : 0,
-  y : 0,
-  moveLog : [],
-
+  x: 0,
+  y: 0,
+  moveLog: []
 };
 
-
-
 // ======================
-function turnLeft(rover){
-  
+function turnLeft(rover) {
   console.log("turnLeft was called!");
 
-  switch(rover.direction)
-  {
-      case "n" : rover.direction = "w"
+  switch (rover.direction) {
+    case "n":
+      rover.direction = "w";
       break;
-      case "w" : rover.direction = "s"
+    case "w":
+      rover.direction = "s";
       break;
-      case "s" : rover.direction = "e"
+    case "s":
+      rover.direction = "e";
       break;
-      case "e" : rover.direction = "n"
+    case "e":
+      rover.direction = "n";
       break;
-      default : rover.direction 
-    
+    default:
+      rover.direction;
   }
-
 }
 
-function turnRight(rover){
-
+function turnRight(rover) {
   console.log("turnRight was called!");
-  switch(rover.direction)
-  {
-      case "n" : rover.direction = "e"
+  switch (rover.direction) {
+    case "n":
+      rover.direction = "e";
       break;
-      case "e" : rover.direction = "s"
+    case "e":
+      rover.direction = "s";
       break;
-      case "s" : rover.direction = "w"
+    case "s":
+      rover.direction = "w";
       break;
-      case "w" : rover.direction = "n"
+    case "w":
+      rover.direction = "n";
       break;
-      default : rover.direction 
-    
+    default:
+      rover.direction;
   }
-
-  
 }
 
-function moveForward(rover){
-  console.log("moveForward was called")
-  switch(rover.direction)
-  {
-      case "n" : rover.y += 1
+function moveForward(rover) {
+  console.log("moveForward was called");
+  switch (rover.direction) {
+    case "n":
+      rover.y += 1;
       break;
-      case "w" : rover.x -= 1
+    case "w":
+      rover.x -= 1;
       break;
-      case "s" : rover.y -= 1
+    case "s":
+      rover.y -= 1;
       break;
-      case "e" : rover.x += 1
+    case "e":
+      rover.x += 1;
       break;
-      default : rover.direction 
-    
+    default:
+      rover.direction;
   }
   console.log("x: " + rover.x, "y:" + rover.y);
 }
 
-function commands(comm){
-  for (i=0; i <= comm.length; i++){
-    if (comm[i] === "f"){
+function commands(comm) {
+  for (i = 0; i <= comm.length; i++) {
+    if (comm[i] === "f") {
       moveForward(rover);
-    }else
-    if (comm[i] === "r") {
+    } else if (comm[i] === "r") {
       turnRight(rover);
-    }else
-    if (comm[i] === "l") {
+    } else if (comm[i] === "l") {
       turnLeft(rover);
     }
-    
   }
-  rover.moveLog.push(["x: "+ rover.x, "y: " + rover.y])
-  console.log(rover.moveLog)
+  rover.moveLog.push(["x: " + rover.x, "y: " + rover.y]);
+  console.log(rover.moveLog);
 }
